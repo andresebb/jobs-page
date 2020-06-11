0e431b9751fe9d19b50637d3dc5c7bba8d5f1d44
andres <aebb001@gmail.com>
2020-06-11 19:51:34 -0400

    anadiando animacion con js en las categorias, verificar como podemos mejorar con el overflow

--- js/action.js
+++ js/action.js
@@ -2,6 +2,14 @@ const $arrowLeft = document.getElementById("arrow-left");
 const $arrowRight = document.getElementById("arrow-right");
 const $firstElement = document.getElementById("elemento-first");
 const $lastElement = document.getElementById("elemento-last");
+const $categorias = document.getElementById("hero__categories__images");
 
+$arrowRight.addEventListener("click", (event) => {
+    $categorias.classList.add("active");
+    $categorias.classList.remove("noactive");
+})
 
+$arrowLeft.addEventListener("click", (evento) => {
+    $categorias.classList.toggle("noactive");
+})
 
