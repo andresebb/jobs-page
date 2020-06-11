fa75f7ed061c4f073f51caf0271e48959b25f9e3
andres <aebb001@gmail.com>
2020-06-10 20:44:08 -0400

    adding js

---
+++ js/action.js
@@ -0,0 +1,7 @@
+const $arrowLeft = document.getElementById("arrow-left");
+const $arrowRight = document.getElementById("arrow-right");
+const $firstElement = document.getElementById("elemento-first");
+const $lastElement = document.getElementById("elemento-last");
+
+
+
